b55b7f687b15284986f14bb5af0b7f16dc7e9e16
EllfredTheHuman <ellfred189@gmail.com>
2026-08-06 14:21:34 +1000

    Refactor electricity questions format

--- questions/electricity.js
+++ questions/electricity.js
@@ -1,44 +1,542 @@
 const electricityQuestions = [
 
-    {
-        question: "What is electricity?",
+{
+question:"What is electricity?",
+answers:[
+"The movement of electric charge",
+"A type of food",
+"A living thing",
+"A type of sound"
+],
+correct:0
+},
 
-        answers: [
-            "The movement of electric charge",
-            "A type of food",
-            "A living thing",
-            "A type of sound"
-        ],
+{
+question:"What unit is used to measure electric current?",
+answers:[
+"Volts",
+"Amps",
+"Watts",
+"Ohms"
+],
+correct:1
+},
 
-        correct: 0
-    },
+{
+question:"What device provides electrical energy?",
+answers:[
+"Battery",
+"Switch",
+"Wire",
+"Bulb"
+],
+correct:0
+},
 
+{
+question:"What is electric current?",
+answers:[
+"The flow of electric charge",
+"The brightness of a bulb",
+"The size of a battery",
+"The amount of wire"
+],
+correct:0
+},
 
-    {
-        question: "What unit is used to measure electric current?",
+{
+question:"What unit is voltage measured in?",
+answers:[
+"Amps",
+"Volts",
+"Ohms",
+"Watts"
+],
+correct:1
+},
 
-        answers: [
-            "Volts",
-            "Amps",
-            "Watts",
-            "Ohms"
-        ],
+{
+question:"What does voltage measure?",
+answers:[
+"The push that moves electric charges",
+"The amount of resistance",
+"The speed of light",
+"The amount of wire"
+],
+correct:0
+},
 
-        correct: 1
-    },
+{
+question:"What unit is resistance measured in?",
+answers:[
+"Ohms",
+"Volts",
+"Amps",
+"Joules"
+],
+correct:0
+},
 
+{
+question:"What does resistance do in a circuit?",
+answers:[
+"Opposes the flow of current",
+"Creates electricity",
+"Stores food",
+"Produces light"
+],
+correct:0
+},
 
-    {
-        question: "What device provides electrical energy?",
+{
+question:"What is needed for a circuit to work?",
+answers:[
+"A complete pathway",
+"Only a battery",
+"Only a bulb",
+"Only a switch"
+],
+correct:0
+},
 
-        answers: [
-            "Battery",
-            "Switch",
-            "Wire",
-            "Bulb"
-        ],
+{
+question:"What happens in an open circuit?",
+answers:[
+"Current cannot flow",
+"Current flows faster",
+"The battery grows",
+"The bulb gets brighter"
+],
+correct:0
+},
 
-        correct: 0
-    }
+{
+question:"What happens in a closed circuit?",
+answers:[
+"Current can flow",
+"The circuit breaks",
+"No energy exists",
+"The battery disappears"
+],
+correct:0
+},
+
+{
+question:"What material is a good conductor?",
+answers:[
+"Copper",
+"Plastic",
+"Rubber",
+"Glass"
+],
+correct:0
+},
+
+{
+question:"What is a conductor?",
+answers:[
+"A material that allows electricity to flow",
+"A material that blocks electricity",
+"A battery type",
+"A circuit symbol"
+],
+correct:0
+},
+
+{
+question:"What is an insulator?",
+answers:[
+"A material that resists electric flow",
+"A material that creates electricity",
+"A type of battery",
+"A type of current"
+],
+correct:0
+},
+
+{
+question:"Which is an insulator?",
+answers:[
+"Plastic",
+"Copper",
+"Aluminium",
+"Silver"
+],
+correct:0
+},
+
+{
+question:"What is the purpose of a switch?",
+answers:[
+"To open or close a circuit",
+"To create electricity",
+"To measure voltage",
+"To store energy"
+],
+correct:0
+},
+
+{
+question:"What does a battery provide in a circuit?",
+answers:[
+"Electrical energy",
+"Resistance",
+"Light",
+"Sound"
+],
+correct:0
+},
+
+{
+question:"What does a bulb do in a circuit?",
+answers:[
+"Converts electrical energy into light",
+"Creates electricity",
+"Stops current",
+"Measures resistance"
+],
+correct:0
+},
+
+{
+question:"What happens if a bulb breaks in a series circuit?",
+answers:[
+"The whole circuit stops working",
+"Nothing changes",
+"The battery doubles",
+"Current increases"
+],
+correct:0
+},
+
+{
+question:"What type of circuit has components connected in one path?",
+answers:[
+"Series circuit",
+"Parallel circuit",
+"Open circuit",
+"Broken circuit"
+],
+correct:0
+},
+
+{
+question:"What type of circuit has multiple pathways?",
+answers:[
+"Parallel circuit",
+"Series circuit",
+"Open circuit",
+"Static circuit"
+],
+correct:0
+},
+
+{
+question:"Why are parallel circuits useful in homes?",
+answers:[
+"Devices can work independently",
+"They use no electricity",
+"They cannot break",
+"They remove resistance"
+],
+correct:0
+},
+
+{
+question:"What happens to current in a series circuit if more bulbs are added?",
+answers:[
+"Current decreases",
+"Current increases forever",
+"Nothing changes",
+"The battery disappears"
+],
+correct:0
+},
+
+{
+question:"Static electricity is caused by:",
+answers:[
+"An imbalance of electric charges",
+"Moving water",
+"Heat only",
+"Magnetism only"
+],
+correct:0
+},
+
+{
+question:"Which particle has a negative charge?",
+answers:[
+"Electron",
+"Proton",
+"Neutron",
+"Atom"
+],
+correct:0
+},
+
+{
+question:"Which particle has a positive charge?",
+answers:[
+"Proton",
+"Electron",
+"Neutron",
+"Cell"
+],
+correct:0
+},
+
+{
+question:"Which particle has no charge?",
+answers:[
+"Neutron",
+"Electron",
+"Proton",
+"Ion"
+],
+correct:0
+},
+
+{
+question:"Like charges:",
+answers:[
+"Repel each other",
+"Attract each other",
+"Disappear",
+"Create matter"
+],
+correct:0
+},
+
+{
+question:"Opposite charges:",
+answers:[
+"Attract each other",
+"Repel each other",
+"Stop existing",
+"Become magnets"
+],
+correct:0
+},
+
+{
+question:"What is electrical energy?",
+answers:[
+"Energy transferred by electric charges",
+"Energy from food only",
+"Sound energy",
+"Movement of air"
+],
+correct:0
+},
+
+{
+question:"What does an ammeter measure?",
+answers:[
+"Current",
+"Voltage",
+"Resistance",
+"Temperature"
+],
+correct:0
+},
+
+{
+question:"What does a voltmeter measure?",
+answers:[
+"Voltage",
+"Current",
+"Resistance",
+"Power"
+],
+correct:0
+},
+
+{
+question:"Where is an ammeter placed in a circuit?",
+answers:[
+"In series",
+"In parallel",
+"Outside the circuit",
+"Under the battery"
+],
+correct:0
+},
+
+{
+question:"Where is a voltmeter placed in a circuit?",
+answers:[
+"In parallel",
+"In series",
+"Inside the battery",
+"Outside the room"
+],
+correct:0
+},
+
+{
+question:"What is power?",
+answers:[
+"The rate electrical energy is transferred",
+"The amount of wire",
+"The size of a circuit",
+"The number of electrons"
+],
+correct:0
+},
+
+{
+question:"What unit is power measured in?",
+answers:[
+"Watts",
+"Volts",
+"Amps",
+"Ohms"
+],
+correct:0
+},
+
+{
+question:"What happens when resistance increases?",
+answers:[
+"Current decreases",
+"Current increases",
+"Voltage disappears",
+"Energy is created"
+],
+correct:0
+},
+
+{
+question:"What energy transformation happens in a fan?",
+answers:[
+"Electrical to movement",
+"Movement to electrical",
+"Light to sound",
+"Chemical to heat"
+],
+correct:0
+},
+
+{
+question:"What energy transformation happens in a toaster?",
+answers:[
+"Electrical to heat",
+"Heat to electricity",
+"Light to movement",
+"Sound to chemical"
+],
+correct:0
+},
+
+{
+question:"Why are electrical safety rules important?",
+answers:[
+"To prevent injury and damage",
+"To make electricity disappear",
+"To increase resistance",
+"To stop batteries working"
+],
+correct:0
+},
+
+{
+question:"Why should you not touch electrical devices with wet hands?",
+answers:[
+"Water can allow current to flow through you",
+"It makes electricity disappear",
+"It charges the device",
+"It increases battery life"
+],
+correct:0
+},
+
+{
+question:"What is a short circuit?",
+answers:[
+"An unwanted low-resistance path for current",
+"A longer wire",
+"A battery type",
+"A light bulb"
+],
+correct:0
+},
+
+{
+question:"What device protects circuits from too much current?",
+answers:[
+"Fuse",
+"Bulb",
+"Switch",
+"Wire"
+],
+correct:0
+},
+
+{
+question:"What does a fuse do?",
+answers:[
+"Breaks the circuit if current is too high",
+"Creates electricity",
+"Stores energy",
+"Increases voltage"
+],
+correct:0
+},
+
+{
+question:"Renewable electricity comes from sources that:",
+answers:[
+"Can be replaced naturally",
+"Never run out because they are unlimited machines",
+"Are always fossil fuels",
+"Cannot produce energy"
+],
+correct:0
+},
+
+{
+question:"Solar panels convert:",
+answers:[
+"Light energy into electrical energy",
+"Electrical energy into sound",
+"Heat into food",
+"Movement into light"
+],
+correct:0
+},
+
+{
+question:"Wind turbines convert:",
+answers:[
+"Movement energy into electrical energy",
+"Light into sound",
+"Electricity into wind",
+"Heat into water"
+],
+correct:0
+},
+
+{
+question:"What is a circuit diagram?",
+answers:[
+"A drawing using symbols to represent a circuit",
+"A photo of a battery",
+"A list of wires",
+"A type of electricity"
+],
+correct:0
+},
+
+{
+question:"Why are symbols used in circuit diagrams?",
+answers:[
+"To make circuits easier to understand",
+"To create electricity",
+"To increase voltage",
+"To stop current"
+],
+correct:0
+}
 
 ];
