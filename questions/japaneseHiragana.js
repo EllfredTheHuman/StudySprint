cd155044dde956b160a4c2e92bd976cd099c58a3
EllfredTheHuman <ellfred189@gmail.com>
2026-08-07 16:44:45 +1000

    Add Japanese Hiragana questions for quiz

--- questions/japaneseHiragana.js
+++ questions/japaneseHiragana.js
@@ -1,1 +1,297 @@
+const japaneseHiraganaQuestions = [
 
+{
+question:"What sound does あ represent?",
+answers:["a","i","u","e"],
+correct:0
+},
+
+{
+question:"What sound does い represent?",
+answers:["a","i","u","o"],
+correct:1
+},
+
+{
+question:"What sound does う represent?",
+answers:["u","e","o","a"],
+correct:0
+},
+
+{
+question:"What sound does え represent?",
+answers:["e","i","a","u"],
+correct:0
+},
+
+{
+question:"What sound does お represent?",
+answers:["o","a","e","u"],
+correct:0
+},
+
+{
+question:"What sound does か represent?",
+answers:["ka","ki","ku","ko"],
+correct:0
+},
+
+{
+question:"What sound does き represent?",
+answers:["ki","ka","ke","ku"],
+correct:0
+},
+
+{
+question:"What sound does く represent?",
+answers:["ku","ko","ki","ka"],
+correct:0
+},
+
+{
+question:"What sound does け represent?",
+answers:["ke","ka","ki","ko"],
+correct:0
+},
+
+{
+question:"What sound does こ represent?",
+answers:["ko","ku","ke","ka"],
+correct:0
+},
+
+{
+question:"What sound does さ represent?",
+answers:["sa","shi","su","so"],
+correct:0
+},
+
+{
+question:"What sound does し represent?",
+answers:["shi","sa","se","su"],
+correct:0
+},
+
+{
+question:"What sound does す represent?",
+answers:["su","shi","sa","so"],
+correct:0
+},
+
+{
+question:"What sound does せ represent?",
+answers:["se","so","sa","shi"],
+correct:0
+},
+
+{
+question:"What sound does そ represent?",
+answers:["so","su","se","sa"],
+correct:0
+},
+
+{
+question:"What sound does た represent?",
+answers:["ta","te","to","ti"],
+correct:0
+},
+
+{
+question:"What sound does ち represent?",
+answers:["chi","ta","tsu","te"],
+correct:0
+},
+
+{
+question:"What sound does つ represent?",
+answers:["tsu","chi","ta","to"],
+correct:0
+},
+
+{
+question:"What sound does て represent?",
+answers:["te","ta","to","tsu"],
+correct:0
+},
+
+{
+question:"What sound does と represent?",
+answers:["to","ta","te","chi"],
+correct:0
+},
+
+{
+question:"What sound does な represent?",
+answers:["na","ni","nu","no"],
+correct:0
+},
+
+{
+question:"What sound does に represent?",
+answers:["ni","na","ne","nu"],
+correct:0
+},
+
+{
+question:"What sound does ぬ represent?",
+answers:["nu","no","na","ne"],
+correct:0
+},
+
+{
+question:"What sound does ね represent?",
+answers:["ne","ni","na","no"],
+correct:0
+},
+
+{
+question:"What sound does の represent?",
+answers:["no","nu","ne","na"],
+correct:0
+},
+
+{
+question:"What sound does は represent?",
+answers:["ha","hi","ho","he"],
+correct:0
+},
+
+{
+question:"What sound does ひ represent?",
+answers:["hi","ha","fu","he"],
+correct:0
+},
+
+{
+question:"What sound does ふ represent?",
+answers:["fu","ha","ho","hi"],
+correct:0
+},
+
+{
+question:"What sound does へ represent?",
+answers:["he","ha","ho","hi"],
+correct:0
+},
+
+{
+question:"What sound does ほ represent?",
+answers:["ho","fu","ha","he"],
+correct:0
+},
+
+{
+question:"What sound does ま represent?",
+answers:["ma","mi","mu","mo"],
+correct:0
+},
+
+{
+question:"What sound does み represent?",
+answers:["mi","ma","me","mu"],
+correct:0
+},
+
+{
+question:"What sound does む represent?",
+answers:["mu","mo","ma","mi"],
+correct:0
+},
+
+{
+question:"What sound does め represent?",
+answers:["me","mi","ma","mo"],
+correct:0
+},
+
+{
+question:"What sound does も represent?",
+answers:["mo","mu","me","ma"],
+correct:0
+},
+
+{
+question:"What sound does や represent?",
+answers:["ya","yu","yo","ra"],
+correct:0
+},
+
+{
+question:"What sound does ゆ represent?",
+answers:["yu","ya","yo","ri"],
+correct:0
+},
+
+{
+question:"What sound does よ represent?",
+answers:["yo","yu","ya","wa"],
+correct:0
+},
+
+{
+question:"What sound does ら represent?",
+answers:["ra","ri","ru","ro"],
+correct:0
+},
+
+{
+question:"What sound does り represent?",
+answers:["ri","ra","ru","re"],
+correct:0
+},
+
+{
+question:"What sound does る represent?",
+answers:["ru","ra","ri","ro"],
+correct:0
+},
+
+{
+question:"What sound does れ represent?",
+answers:["re","ri","ra","ru"],
+correct:0
+},
+
+{
+question:"What sound does ろ represent?",
+answers:["ro","ru","re","ra"],
+correct:0
+},
+
+{
+question:"What sound does わ represent?",
+answers:["wa","wo","ra","ya"],
+correct:0
+},
+
+{
+question:"What sound does を represent?",
+answers:["wo/o","wa","yo","no"],
+correct:0
+},
+
+{
+question:"What sound does ん represent?",
+answers:["n","m","r","h"],
+correct:0
+},
+
+{
+question:"Which hiragana means 'ne'?",
+answers:["ね","ぬ","に","の"],
+correct:0
+},
+
+{
+question:"Which hiragana means 'mi'?",
+answers:["み","ま","む","め"],
+correct:0
+},
+
+{
+question:"Which hiragana means 'yo'?",
+answers:["よ","ゆ","や","よ"],
+correct:0
+}
+
+];
