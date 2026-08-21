9a50314a0f27be4fbb044a44da44fac1d2cd40f0
EllfredTheHuman <ellfred189@gmail.com>
2026-08-21 17:10:16 +1000

    Add Firebase Realtime Database configuration

--- firebase.js
+++ firebase.js
@@ -5,15 +5,27 @@
 import { initializeApp } from
     "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
+import { getDatabase } from
+    "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyBi3Ge5_pDiEV-scRC-kptDJoHnHmbdw6s",
     authDomain: "studysprint-67f63.firebaseapp.com",
     projectId: "studysprint-67f63",
     storageBucket: "studysprint-67f63.firebasestorage.app",
     messagingSenderId: "1076120438088",
-    appId: "1:1076120438088:web:c3afbd7ff39ebeaeac1f7d"
+    appId: "1:1076120438088:web:c3afbd7ff39ebeaeac1f7d",
+    databaseURL: "https://studysprint-67f63-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
-export { app };
+const db = getDatabase(app);
+
+
+export {
+    app,
+    db
+};
