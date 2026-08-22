e48f7656c87696a2328ca154f4a8d9713e44cdb7
EllfredTheHuman <ellfred189@gmail.com>
2026-08-22 13:00:31 +1000

    Improve Firebase configuration formatting
    Refactor Firebase configuration and imports for better readability.

--- firebase.js
+++ firebase.js
@@ -1,31 +1,45 @@
-// =========================================================
-// STUDYSPRINT FIREBASE
-// =========================================================
+/* =========================================================
+   STUDYSPRINT FIREBASE
+========================================================= */
 
-import { initializeApp } from
-    "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
-import { getDatabase } from
-    "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
+import {
+    getDatabase
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBi3Ge5_pDiEV-scRC-kptDJoHnHmbdw6s",
-    authDomain: "studysprint-67f63.firebaseapp.com",
-    projectId: "studysprint-67f63",
-    storageBucket: "studysprint-67f63.firebasestorage.app",
-    messagingSenderId: "1076120438088",
-    appId: "1:1076120438088:web:c3afbd7ff39ebeaeac1f7d",
-    databaseURL: "https://studysprint-67f63-default-rtdb.asia-southeast1.firebasedatabase.app"
+
+    apiKey:
+        "AIzaSyBiGe5_pDiEV-scRC-kptDJoHnHmbdw6s",
+
+    authDomain:
+        "studysprint-67f63.firebaseapp.com",
+
+    databaseURL:
+        "https://studysprint-67f63-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+    projectId:
+        "studysprint-67f63",
+
+    storageBucket:
+        "studysprint-67f63.firebasestorage.app",
+
+    messagingSenderId:
+        "1076120438088",
+
+    appId:
+        "1:1076120438088:web:c3afbd7ff39ebeaeac1f7"
+
 };
 
 
-const app = initializeApp(firebaseConfig);
+const app =
+    initializeApp(firebaseConfig);
 
-const db = getDatabase(app);
 
-
-export {
-    app,
-    db
-};
+export const db =
+    getDatabase(app);
